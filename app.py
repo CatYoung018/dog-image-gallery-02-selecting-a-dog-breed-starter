0e1b734485c473d0198a7e75607dc997d494fbeb
cat_young <cyoun002@gmail.com>
2025-10-30 10:53:37 -0400

    feat: add drop-down menu for 1/3/5 images

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 
+import requests
+
 #imports a dictionary of data from dog_breeds.py and "prettifies", or styles, the dog names when they appear in the HTML page
 from dog_breeds import prettify_dog_breed
 
@@ -14,14 +16,18 @@ def check_breed(breed):
 def dog_image_gallery():
   errors = []
   if request.method == "POST":
-     breed = request.form.get("breed")
-     if not breed:
-        errors.append("Oops! Please choose a dog breed.")
-     if breed:
-        response = requests.get("https://dog.ceo/api/breed/" + check_breed(breed) + "/images/random/30")
-        data = response.json()
-        dog_images = data["message"]
-  return render_template("dogs.html")
+    breed = request.form.get("breed")
+    number = request.form.get("number")
+    if not breed:
+      errors.append("Oops! Please choose a breed.")
+    if not number:
+      errors.append("Oops! Please choose number.")
+    if breed:
+      response = requests.get("https://dog.ceo/api/breed/" + check_breed(breed) + "/images/random/" + number)
+      data = response.json()
+      dog_images = data["message"]
+      return render_template("dogs.html", images=dog_images, breed=prettify_dog_breed(breed), errors=[])
+  return render_template("dogs.html", images=[], breed="", errors=errors)
 
 
 app.debug = True
