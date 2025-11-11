48b8e4afb533e9842c17caae5d1567b8246acc70
cat_young <cyoun002@gmail.com>
2025-11-11 07:33:50 -0500

    Add OR button for random images and correct merge issues

--- app.py
+++ app.py
@@ -1,27 +1,39 @@
 from flask import Flask, render_template, request
 
-#imports a dictionary of data from dog_breeds.py and "prettifies", or styles, the dog names when they appear in the HTML page
+import requests
+
 from dog_breeds import prettify_dog_breed
 
 # Initialize the Flask application
 app = Flask(__name__)
 
-#function adds a dash in the URL between breed names with multiple words like miniature poodle
 def check_breed(breed):
   return "/".join(breed.split("-"))
 
 @app.route("/", methods=["GET","POST"])
-def dog_image_gallery():
+def dog_images_gallery():
   errors = []
   if request.method == "POST":
-     breed = request.form.get("breed")
-     if not breed:
-        errors.append("Oops! Please choose a dog breed.")
-     if breed:
-        response = requests.get("https://dog.ceo/api/breed/" + check_breed(breed) + "/images/random/30")
-        data = response.json()
-        dog_images = data["message"]
-  return render_template("dogs.html")
+    breed = request.form.get("breed")
+    number = request.form.get("number")
+    if not breed:
+      errors.append("Oops! Please choose a breed.")
+    if not number:
+      errors.append("Oops! Please choose a number.")
+    if breed and number:
+      response = requests.get("https://dog.ceo/api/breed/" + check_breed(breed) + "/images/random/" + number)
+      data = response.json()
+      dog_images = data["message"]
+      return render_template("dogs.html", images=dog_images, breed=prettify_dog_breed(breed), errors=[])
+  return render_template("dogs.html", images=[], breed="", errors=errors)
+
+@app.route("/random", methods=["POST"])
+def get_random():
+    response = requests.get("https://dog.ceo/api/breeds/image/random")
+    data = response.json()
+    dog_images = [data["message"]]
+    return render_template("dogs.html", images=dog_images)
+
 
 
 app.debug = True
